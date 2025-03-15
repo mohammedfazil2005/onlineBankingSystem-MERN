a1005ae6357ff111cd0107c53284916c1620b783
Mohammedfazil <mohammedfazil20005@gmail.com>
2025-03-16 01:20:29 +0530

    updating admin dashboard

--- src/pages/admin/homeadminright/homeadmindashboard/HomeAdminDashboard.jsx
+++ src/pages/admin/homeadminright/homeadmindashboard/HomeAdminDashboard.jsx
@@ -1,81 +1,50 @@
 import React, { useState } from 'react'
 import './HomeAdminDashboard.css'
-import { BarChart } from '@mui/x-charts/BarChart';
-import { PieChart } from '@mui/x-charts';
+import { Line } from 'react-chartjs-2'
+import { Chart as ChartJS,CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend } from 'chart.js'
+
+
+ChartJS.register(CategoryScale,LinearScale,PointElement,LineElement,Title,Tooltip,Legend)
 
 const HomeAdminDashboard = () => {
-    const [state,setState]=useState({
-        series: [{
-            name: 'Net Profit',
-            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-          }, {
-            name: 'Revenue',
-            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-          }, {
-            name: 'Free Cash Flow',
-            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-          }],
-          options: {
-            chart: {
-              type: 'bar',
-              height: 350
-            },
-            plotOptions: {
-              bar: {
-                horizontal: false,
-                columnWidth: '55%',
-                borderRadius: 5,
-                borderRadiusApplication: 'end'
-              },
-            },
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              show: true,
-              width: 2,
-              colors: ['transparent']
-            },
-            xaxis: {
-              categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-            },
-            yaxis: {
-              title: {
-                text: '$ (thousands)'
-              }
-            },
-            fill: {
-              opacity: 1
-            },
-            tooltip: {
-              y: {
-                formatter: function (val) {
-                  return "$ " + val + " thousands"
-                }
-              }
-            }
-          },
-        
-        
-      
-    })
+
+  const Linedata={
+    labels:["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"],
+    datasets:[
+      {
+      label:"Credit Card",
+      data:[10,5],
+      borderColor:"red"
+     },
+     {
+      label:"Debit Card",
+      data:[3,15],
+      borderColor:"yellow"
+     }
+  ]
+  }
+
+  const Lineoptions = {
+    responsive: true,  // Enables responsiveness
+    maintainAspectRatio: false, // Allows custom width & height
+  };
+
+
+  
 
     return (
         <div className='home-dashboard-parent'>
             <div className="heading-dashboard">
                 <h1>Dashboard</h1>
+                <p>Welcome to dashboard</p>
             </div>
-            <div className="header-admin-dashboard">
-                <div className='header-admin-dashboard-left'>
-                    
-                   
-                </div>
-               
+
+            <div style={{ width: "90%", height: "auto" }}>
+              <Line data={Linedata} options={Lineoptions}style={{height:'300px'}}/>
             </div>
+
+
             <div className="admin-chart-parent" style={{width:"100%"}}>
-                
-            </div>
-            <div className="admin-recent-transactions">
             <div className="user-transaction-history-full">
                 <div className="user-transaction-history-list-heading">
                     <h2>Transaction history</h2>
@@ -88,7 +57,7 @@ const HomeAdminDashboard = () => {
             <p>Amount</p>
             <p>Status</p>
             <p>Date</p>
-            <p>Details</p>
+            <p>Download</p>
         </div>
         <div className="user-transaction-list">
             <p>1</p>
@@ -96,10 +65,11 @@ const HomeAdminDashboard = () => {
             <p>200</p>
             <p>Processing</p>
             <p>Wed 11:00pm</p>
-           <button>Details</button>
+           <button>Download</button>
         </div>
             </div>
             </div>
+            
         </div>
     )
 }
