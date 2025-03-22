69a458b7e16637a8d78129a272723e2489b86ceb
Mohammedfazil <mohammedfazil20005@gmail.com>
2025-03-22 15:38:42 +0530

    updated cards

--- src/pages/admin/homeadminright/homeadmincreditCardManage/HomeAdminCreditCardManage.jsx
+++ src/pages/admin/homeadminright/homeadmincreditCardManage/HomeAdminCreditCardManage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './HomeAdminCreditCardManage.css'
 const HomeAdminCreditCardManage = () => {
+
+    
     return (
         <div className='home-admin-credit-manage-parent'>
             <div className="heading-dashboard">
@@ -8,7 +10,7 @@ const HomeAdminCreditCardManage = () => {
                 <p>Review and manage all credit cards assigned to users within the banking system.</p>
             </div>
             <div className="user-card-div-main">
-                <div className='main-card' style={{ background: 'url(https://img.freepik.com/free-vector/red-wavy-background_1189-269.jpg?t=st=1742144590~exp=1742148190~hmac=58499faebca52b4edf5bbae339e2b93062d3cbd7b10432b9bf782e4c4da83de8&w=826)' }}>
+                <div className='main-card' style={{ background: 'url(https://img.freepik.com/premium-vector/gold-luxury-background-theme_23-2148605021.jpg)' }}>
                     <div className='main-card-heading'>
                         <h5>Bank Ai</h5>
                         <p>Debit</p>
@@ -19,10 +21,15 @@ const HomeAdminCreditCardManage = () => {
                     </div>
                     <div className='main-card-card-details'>
                         <p><span>1234</span><span>1234</span> <span>1234</span></p>
-                       <button className='btn'>view</button>
+                       <button className='btn text-light'>view</button>
                     </div>
                    
                 </div>
+
+               
+
+
+                
 
                 
             </div>
